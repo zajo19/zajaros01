71506aff884e1ae26a4905c8a458912faae35969
zajo19 <andrej.zajaros.az@gmail.com>
2024-06-04 09:25:30 +0200

    Update connect.php

--- connect.php
+++ connect.php
@@ -1,7 +1,7 @@
 <?php
 $servername = "localhost";
-$username = "your_username";
-$password = "your_password";
+$username = "userdb";
+$password = "databaza";
 $dbname = "northwindmysql";
 
 // Create connection
