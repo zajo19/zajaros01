1d320ba5e55583b9ee87d454dd23a7b454a9ac98
zajo19 <andrej.zajaros.az@gmail.com>
2024-06-18 13:40:07 +0200

    Update connect.php

--- connect.php
+++ connect.php
@@ -11,6 +11,8 @@ $conn = new mysqli($servername, $username, $password, $dbname);
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
+
+$connection = new mysqli($servername, $username, $password, $dbname);
 if ($connection->connect_error) {
     die("Connection failed: " . $connection->connect_error);
 }
